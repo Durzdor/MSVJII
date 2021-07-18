457965b00262244d8f6d342db91a3407ddd38645
Durzdor <maxiperez024@hotmail.com>
2021-07-17 23:42:52 -0300

    MainMenuFix
    +Help Screen texts
    +Help Screen icons

--- Project/Assets/_OnUse/Scripts/StrokeManager.cs
+++ Project/Assets/_OnUse/Scripts/StrokeManager.cs
@@ -3,65 +3,58 @@ using UnityEngine;
 
 public class StrokeManager : MonoBehaviour
 {
-    public enum StartingAngle { ZForward = 0, XForward = 90, ZBackward = 180, XBackward = 270 };
+    public enum StartingAngle
+    {
+        ZForward = 0,
+        XForward = 90,
+        ZBackward = 180,
+        XBackward = 270
+    };
 
     [Tooltip("A que direccion apunta al principio")]
-    public StartingAngle startingAngle; 
+    public StartingAngle startingAngle;
+
     public StrokeState StrokeMode { get; protected set; }
 
     public delegate void MaxStrokesReached();
+
     public MaxStrokesReached OnMaxStrokesReached;
-
-
     [SerializeField] private int maxStrokes;
-
     [SerializeField] private float uiArrowYOffset;
-
     private GameObject ballGO;
     private Ball ball;
     private Rigidbody playerBallRB;
-
     [SerializeField] private AudioSource audioSrc;
-  
     private float strokeAngle;
-
     [SerializeField] private GameObject hitArrow;
+
     public float StrokeAngle
     {
-        get
-        {
-            return strokeAngle;
-        }
+        get { return strokeAngle; }
         protected set
         {
             strokeAngle = value;
-            hitArrow.transform.rotation = Quaternion.Euler (0,strokeAngle + 180,0);
+            hitArrow.transform.rotation = Quaternion.Euler(0, strokeAngle + 180, 0);
             //ui.ChangeArrowAngle(strokeAngle,Camera.main.transform.rotation.eulerAngles.y);
         }
-        
     }
 
-    
-
     public float StrokeForce { get; protected set; }
-    public float StrokeForcePerc { get { return StrokeForce / (MaxStrokeForce * currentGolfClub.GeneralStrength); } }
+
+    public float StrokeForcePerc
+    {
+        get { return StrokeForce / (MaxStrokeForce * currentGolfClub.GeneralStrength); }
+    }
 
     public int StrokeCount { get; protected set; }
 
     [SerializeField] private float angleChangeSpeed = 100;
-
     private float strokeForceFillSpeed = 5f;
     [SerializeField] private float strikeFillSpeed;
-
-
-
     [SerializeField] float MaxStrokeForce = 10f;
-
     private UI ui;
-
     [SerializeField] private GolfClub[] golfClubsAvailable;
     private GolfClub currentGolfClub;
-    private int golfClubIndex;
 
     public enum StrokeState
     {
@@ -78,11 +71,9 @@ public class StrokeManager : MonoBehaviour
         playerBallRB = ballGO.GetComponent<Rigidbody>();
     }
 
-
     private void EnableArrow(bool isEnabled)
     {
-
-        hitArrow.SetActive(isEnabled);        
+        hitArrow.SetActive(isEnabled);
         hitArrow.transform.position = playerBallRB.transform.position + new Vector3(0, uiArrowYOffset, 0);
     }
 
@@ -124,32 +115,17 @@ public class StrokeManager : MonoBehaviour
         FindPlayerBall();
         GetUI();
         StrokeCount = 0;
-        StrokeAngle = (float)startingAngle;
+        StrokeAngle = (float) startingAngle;
         ChangeState(StrokeState.Aiming);
-        golfClubIndex = -1;        
-        ChangeGolfClub();
+        currentGolfClub = golfClubsAvailable[0];
+        UpdateGolfClubUI();
         GameManager.instance.StrokeManagerRef(this);
         audioSrc = GetComponent<AudioSource>();
     }
 
-  private void GetUI()
+    private void GetUI()
     {
-        ui = GameObject.FindGameObjectWithTag("UI").GetComponent <UI>();
-    }
-
-    private void ChangeGolfClub()
-    {
-        golfClubIndex++;
-
-        if (golfClubIndex >= golfClubsAvailable.Length)
-        {
-            golfClubIndex = 0;
-        }
-
-        currentGolfClub = golfClubsAvailable[golfClubIndex];
-
-        UpdateGolfClubUI();
-
+        ui = GameObject.FindGameObjectWithTag("UI").GetComponent<UI>();
     }
 
     private void UpdateGolfClubUI()
@@ -159,7 +135,8 @@ public class StrokeManager : MonoBehaviour
 
     private void HitBall()
     {
-        Vector3 forceVec = StrokeForce * Vector3.forward + StrokeForce * Vector3.up * currentGolfClub.VerticalFactorStrength;
+        Vector3 forceVec = StrokeForce * Vector3.forward +
+                           StrokeForce * Vector3.up * currentGolfClub.VerticalFactorStrength;
         playerBallRB.AddForce(Quaternion.Euler(0, StrokeAngle, 0) * forceVec, ForceMode.Impulse);
         ball.InitialForce = forceVec.magnitude;
         StrokeForce = 0;
@@ -185,9 +162,22 @@ public class StrokeManager : MonoBehaviour
 
                 StrokeAngle += Input.GetAxis("Horizontal") * angleChangeSpeed * Time.deltaTime;
 
-                if (Input.GetKeyDown(KeyCode.Tab))
+                if (Input.GetKeyDown(KeyCode.Alpha1))
                 {
-                    ChangeGolfClub();
+                    currentGolfClub = golfClubsAvailable[0];
+                    UpdateGolfClubUI();
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha2))
+                {
+                    currentGolfClub = golfClubsAvailable[1];
+                    UpdateGolfClubUI();
+                }
+
+                if (Input.GetKeyDown(KeyCode.Alpha3))
+                {
+                    currentGolfClub = golfClubsAvailable[2];
+                    UpdateGolfClubUI();
                 }
 
                 if (Input.GetButtonUp("Fire1"))
@@ -199,7 +189,8 @@ public class StrokeManager : MonoBehaviour
 
             case StrokeState.ForceSet:
 
-                StrokeForce += (strokeForceFillSpeed * Input.mouseScrollDelta.y * strikeFillSpeed) * Time.deltaTime * currentGolfClub.GeneralStrength;
+                StrokeForce += (strokeForceFillSpeed * Input.mouseScrollDelta.y * strikeFillSpeed) * Time.deltaTime *
+                               currentGolfClub.GeneralStrength;
 
                 if (StrokeForce > MaxStrokeForce * currentGolfClub.GeneralStrength)
                 {
@@ -229,6 +220,11 @@ public class StrokeManager : MonoBehaviour
 
             case StrokeState.Move:
                 CheckRollingStatus();
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    if (ball.Dissapearing) return;
+                    ball.ToKnownGoodPosition();
+                }
 
                 break;
             default:
@@ -244,12 +240,4 @@ public class StrokeManager : MonoBehaviour
             ChangeState(StrokeState.Aiming);
         }
     }
-
-   
-
-   
-
-
-
-    
 }
