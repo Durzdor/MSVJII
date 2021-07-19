21dc3c00131c10b6104565181d3da869494d50db
eruandou <zambra_nacho@hotmail.com>
2021-07-19 15:17:58 -0300

    Pendulum crane

--- Project/Assets/_OnUse/Scripts/Pendulum.cs
+++ Project/Assets/_OnUse/Scripts/Pendulum.cs
@@ -10,6 +10,7 @@ public class Pendulum : MonoBehaviour
     [SerializeField] float deformationAmount;
     private Quaternion mainRotation;
     private Vector3 secondaryRotation;
+     private Vector3 initialRotation;
     public enum Pivot
     {
         X,
@@ -18,24 +19,28 @@ public class Pendulum : MonoBehaviour
     }
 
     public Pivot pivotingAxis;
+    private void Start()
+    {
+        //initialRotation = transform.rotation.eulerAngles;
+    }
 
     private void FixedUpdate()
     {
-        float angle = MaxAngleDeflection * Mathf.Sin(Time.time * SpeedOfPendulum);
+        float angle = MaxAngleDeflection * Mathf.Sin(Time.time * SpeedOfPendulum) ;
         float deformatedAngle = MaxAngleDeflection * Mathf.Sin(Time.time * SpeedOfPendulum + deformationAmount);
         float deformation = deformatedAngle * Time.deltaTime * deformationAmount;
         switch (pivotingAxis)
         {
             case Pivot.X:
-                mainRotation = Quaternion.Euler(angle, 0, 0);
+                mainRotation = Quaternion.Euler(angle, transform.rotation.y, transform.rotation.z);
                 secondaryRotation = new Vector3(0, deformation, deformation);
                 break;
             case Pivot.Y:
-                mainRotation = Quaternion.Euler(0, angle, 0);
+                mainRotation = Quaternion.Euler(transform.rotation.x, angle, transform.rotation.z);
                 secondaryRotation = new Vector3(deformation, 0, deformation);
                 break;
             case Pivot.Z:
-                mainRotation = Quaternion.Euler(0, 0, angle);
+                mainRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, angle);
                 secondaryRotation = new Vector3(deformation, deformation, 0);
                 break;
             default:
